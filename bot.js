0c17432e4d51dcade9a94bf10dc0acb9276e2de9
Paul Szilveszter <szilveszter_paul@yahoo.com>
2022-10-02 12:55:08 +0300

    Made the Bot work locally with node.js

--- bot.js
+++ bot.js
@@ -1,14 +1,38 @@
-let posts =[]
+import fetch from "node-fetch";
+import * as https from 'https';
+import * as fs from 'fs';
 
-class Post{
-    constructor(video_url, audio_url){
+// const https = require('https'); 
+// const fs = require('fs');
+
+
+function downloadFile(URL, filename) {
+
+    const file = fs.createWriteStream(filename);
+    
+    const request = https.get(URL, function (response) {
+        
+        response.pipe(file);
+
+        // after download completed close filestream
+        file.on("finish", () => {
+            file.close();
+            console.log("Download Completed");
+        });
+    });
+}
+
+let posts = []
+
+class Post {
+    constructor(video_url, audio_url) {
         this.video_url = video_url
         this.audio_url = audio_url
     }
-    get getVideoURL(){
+    get getVideoURL() {
         return this.video_url;
     }
-    get getAudioURL(){
+    get getAudioURL() {
         return this.audio_url;
     }
 }
@@ -27,54 +51,55 @@ const subreddit = "funnyvideos";
 
 const responses = [];
 
-const fetchPosts = async (subreddit, afterParam)=>{
+const fetchPosts = async (subreddit, afterParam) => {
     const response = await fetch(
-        `https://www.reddit.com/r/${subreddit}.json?limit=${postsPerRequest}${
-          afterParam ? '&after=' + afterParam : ''
+        `https://www.reddit.com/r/${subreddit}.json?limit=${postsPerRequest}${afterParam ? '&after=' + afterParam : ''
         }`
-      ); 
+    );
 
     const responseJson = await response.json();
 
     responses.push(responseJson);
 
-    if(responseJson.data.after && responses.length < maxRequests){
+    if (responseJson.data.after && responses.length < maxRequests) {
         fetchPosts(subreddit, responseJson.data.after);
         return;
     }
 
     parseResults(responses);
-    
+
     console.log(posts);
+
+    downloadFile(posts[0].video_url, "fisier.mp4");
 };
 
-const parseResults = (responses)=>{
+const parseResults = (responses) => {
     const allPosts = [];
 
-    responses.forEach(response =>{
+    responses.forEach(response => {
         allPosts.push(...response.data.children)
     })
 
-    allPosts.forEach(( {data:{secure_media}} )=>{
-    
-        if(secure_media!=null){
-        
-        
-            if(secure_media.reddit_video != null){
+    allPosts.forEach(({ data: { secure_media } }) => {
 
-               const VIDEO_URL = secure_media.reddit_video.fallback_url;
-               const AUDIO_URL = "https;//v.redd.it/" + VIDEO_URL.split('/')[3]+"/DASH_audio.mp4";
+        if (secure_media != null) {
 
-            //    console.log(`Video URL: ${VIDEO_URL}`);
-            //    console.log(`Audio URL: ${AUDIO_URL}`);
+
+            if (secure_media.reddit_video != null) {
+
+                const VIDEO_URL = secure_media.reddit_video.fallback_url;
+                const AUDIO_URL = "https;//v.redd.it/" + VIDEO_URL.split('/')[3] + "/DASH_audio.mp4";
+
+                //    console.log(`Video URL: ${VIDEO_URL}`);
+                //    console.log(`Audio URL: ${AUDIO_URL}`);
 
                 posts.push(new Post(VIDEO_URL, AUDIO_URL));
             }
 
         }
-        
+
     })
-    
+
 };
 
 fetchPosts(subreddit)
